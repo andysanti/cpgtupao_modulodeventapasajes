62d5ac8f4f15e8734232d8b2f1192c5b31aad67e
Victor Ramirez <Victor Ramirez@AS02>
2015-10-07 21:56:17 -0500

    clases creadas

--- trabajo_upao/src/com/upao/progm/CVenta.java
+++ trabajo_upao/src/com/upao/progm/CVenta.java
@@ -15,4 +15,59 @@ public class CVenta {
     CCliente cliente;
     String fechaVenta;
     boolean reserva;
+    double Total;
+
+    public CItinerario getItinerario() {
+        return itinerario;
+    }
+
+    public void setItinerario(CItinerario itinerario) {
+        this.itinerario = itinerario;
+    }
+
+    public CCliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(CCliente cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(String fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public boolean isReserva() {
+        return reserva;
+    }
+
+    public void setReserva(boolean reserva) {
+        this.reserva = reserva;
+    }
+
+    public double getTotal() {
+        return Total;
+    }
+
+    public void setTotal(double Total) {
+        this.Total = Total;
+    }
+
+    public CVenta() {
+    }
+
+    public CVenta(CItinerario itinerario, CCliente cliente, String fechaVenta, boolean reserva, double Total) {
+        this.itinerario = itinerario;
+        this.cliente = cliente;
+        this.fechaVenta = fechaVenta;
+        this.reserva = reserva;
+        this.Total = Total;
+    }
+    
+    
+    
 }
